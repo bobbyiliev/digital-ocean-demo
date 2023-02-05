9b1d9d07ed69811929b292e30c6d0db00c99b99c
Bobby Iliev <bobby@bobbyiliev.com>
2023-02-05 15:39:28 +0200

    Testing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // enable debug logs in production
+  // https://nextjs.org/docs/messages/enable-swc-debug-logs
+  swcLoaderOptions: {
+    debug: true,
+  },
 }
 
 module.exports = nextConfig
